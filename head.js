964afb06ad69ec4969fa8c1134d5539df51c5972
Jiabao Li <hunterxhunterzhiqiya@126.com>
2020-06-19 02:36:39 +0000

    modified head.js file

--- head.js
+++ head.js
@@ -7,8 +7,11 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
-assertEqual("test", "test");
+var head = function(array) {
+  return array[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
+assertEqual(head(["Hello"]), "Hello");
