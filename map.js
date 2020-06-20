3c69278fbde2eaaa3e2720663bc717b64953784a
Jiabao Li <hunterxhunterzhiqiya@126.com>
2020-06-20 18:35:38 +0000

    add map.js file

---
+++ map.js
@@ -0,0 +1,37 @@
+const eqArrays = function(arr1, arr2) {
+  var result = true;
+
+  if (arr1.length === arr2.length) {
+    for (var i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        result = false;
+      }
+    }
+  } else {
+    result = false;
+  }
+
+  return result;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`\u2b55\u2b55\u2b55 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`\u274c\u274c\u274c Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// actual function
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+// test
+assertArraysEqual(map(words, word => word.length), [6, 7, 2, 5, 3]);
