90825ded2405b35c6a495f5a1cd71d07c835cc00
Jiabao Li <hunterxhunterzhiqiya@126.com>
2020-06-26 20:24:39 +0000

    add test file for eqArrays function and modified the funcion file

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`\u2b55\u2b55\u2b55 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`\u274c\u274c\u274c Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arr1, arr2) {
   var result = true;
@@ -22,8 +16,4 @@ const eqArrays = function(arr1, arr2) {
   return result;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+module.exports = eqArrays;
